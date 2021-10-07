2bf12f3e9b670d02a2b4c2c0c2389dea8478dd61
西左 <78782311+westleft@users.noreply.github.com>
2021-10-07 16:05:23 +0800

    live images

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const section = document.querySelector('section');
 const playerLivesCount = document.querySelector('span');
 let playerLives = 6;
 
-playerLivesCount.textContent = playerLives;
+// playerLivesCount.textContent = playerLives;
+
+let lives = document.querySelector('.lives');
+
+function renderLives() {
+    lives.innerHTML = ''
+    for (i = 0; i < playerLives; i++) {
+        lives.innerHTML += `<img src="./images/heart.png" class="heart" alt="">`
+    }
+};
 
 const getData = () => [
     { imgSrc: './images/01.jpeg', name: '01' },
@@ -35,72 +44,75 @@ const cardGenerator = () => {
     cardData.forEach(() => {
         section.innerHTML += `<div class="card"></div>`;
     })
-    let cards = document.querySelectorAll('.card')
+    let cards = document.querySelectorAll('.card');
 
     cards.forEach((item, index) => {
         item.innerHTML += `<img src="${cardData[index]['imgSrc']}" class="face">`;
         item.innerHTML += `<div class="back"></div>`
 
         item.addEventListener('click', (e) => {
-            item.classList.toggle('toggleCard')
-            checkCard(e)
+            item.classList.toggle('toggleCard');
+            checkCard(e);
         })
-        item.setAttribute('name', cardData[index].name)
+        item.setAttribute('name', cardData[index].name);
         console.log(item);
     })
-
 };
 
 const checkCard = (e) => {
-    const clickedCard = e.target
+    const clickedCard = e.target;
     console.log(clickedCard);
-    clickedCard.classList.add('flipped')
-    const flippedCards = document.querySelectorAll('.flipped')
-    const toggleCard = document.querySelectorAll('.toggleCard')
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped');
+    const toggleCard = document.querySelectorAll('.toggleCard');
 
     if (flippedCards.length === 2) {
         if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
             console.log('match');
             flippedCards.forEach((item) => {
-                item.classList.remove('flipped')
-                item.style.pointerEvents = 'none'
+                item.classList.remove('flipped');
+                item.style.pointerEvents = 'none';
             })
         } else {
             flippedCards.forEach((item) => {
-                item.classList.remove('flipped')
+                item.classList.remove('flipped');
                 setTimeout(() => {
-                    item.classList.remove('toggleCard')
-                }, 1000)
-            })
+                    item.classList.remove('toggleCard');
+                }, 1000);
+            });
             playerLives--;
-            playerLivesCount.textContent = playerLives
+            // playerLivesCount.textContent = playerLives;
+            renderLives();
             if (playerLives == 0) {
                 restart();
             }
         }
-        
     }
     if (toggleCard.length === 16) {
         console.log('winiwnwinnwinwinwi');
+        playerLivesCount.textContent = '贏了!你好棒!'
     }
-}
+};
 
+// 重新開始
 const restart = () => {
     let cardData = randomize();
-    let faces = document.querySelectorAll('.face')
-    let cards = document.querySelectorAll('.card')
-    section.style.pointerEvents = 'none'
+    let faces = document.querySelectorAll('.face');
+    let cards = document.querySelectorAll('.card');
+    section.style.pointerEvents = 'none';
     cardData.forEach((item, index) => {
-        cards[index].classList.remove('toggleCard')
+        cards[index].classList.remove('toggleCard');
 
         setTimeout(() => {
-            cards[index].style.pointerEvents = 'All'
-            faces[index].src = item.imgSrc
-            cards[index].setAttribute('name', item.name)
-            section.style.pointerEvents = 'All'
-        }, 1000)
-    })
-}
+            cards[index].style.pointerEvents = 'All';
+            faces[index].src = item.imgSrc;
+            cards[index].setAttribute('name', item.name);
+            section.style.pointerEvents = 'All';
+        }, 1000);
+    });
+    playerLives = 6;
+    renderLives();
+};
 
-
+renderLives();
 cardGenerator();
